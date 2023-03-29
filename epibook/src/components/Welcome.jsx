e82ee9255e1ae7bf729027ab34af9c46268f12b7
ale89-pit <72412290+ale89-pit@users.noreply.github.com>
2023-03-29 16:54:07 +0200

    modifiche

---
+++ epibook/src/components/Welcome.jsx
@@ -0,0 +1,27 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+function Welcome (){
+    return (
+        <>
+        <div className="jumbotron">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p className="lead">
+          <a className="btn btn-primary btn-lg" href="#" role="button">
+            Learn more
+          </a>
+        </p>
+        
+       
+      </div>
+    </>
+    
+    )
+}
+
+export default Welcome

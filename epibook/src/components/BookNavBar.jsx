ab81360935d859dc8137b4214526e611ae3df6bb
ale89-pit <72412290+ale89-pit@users.noreply.github.com>
2023-03-29 15:23:33 +0200

    save

---
+++ epibook/src/components/BookNavBar.jsx
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/NavBar'
+
+function BookNavBar(){
+    return (
+        <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#home">EpiBook</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Browser</Nav.Link>
+            
+              
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    )
+}
+
+export default BookNavBar

eda02c9b99aa27b9cf3b080375d12a0422999f06
alessio <alessiop942@gmail.com>
2023-04-04 00:27:31 +0200

    crud react

--- epibook/src/components/SingleBook.jsx
+++ epibook/src/components/SingleBook.jsx
@@ -2,40 +2,48 @@ import { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from "react-bootstrap/Col";
+import CommentArea from './CommentArea';
 
 
 
 class SingleBook extends Component {
     state = {
-        isSelected : false,
+        isSelected: false,
     }
     handleClick = (e) => {
+
         this.setState({
-            isSelected :!this.state.isSelected
+            isSelected: !this.state.isSelected
+
         })
+        console.log(e.target)
     }
-    render(){
-       let cardClass = this.state.isSelected ? "border" : "";
+    render() {
+        let cardClass = this.state.isSelected ? "redBorder" : "";
         return (
-            <Col  onClick={this.handleClick} key={this.props.book.asin} xs={6} lg={4} xl={3}>
-            <Card className={"mb-2" + {cardClass}}>
-    
-                <Card.Img variant="top" src={this.props.book.img} />
-                <Card.Body>
-    
-                    <Card.Text className="text-truncate">
-                        {this.props.book.title}
-                    </Card.Text>
-                    <Card.Text>{this.props.book.price} €</Card.Text>
-                    <Button variant="primary">Buy</Button>
-                </Card.Body>
-            </Card>
-        </Col>
-        
+            <Col xs={6} lg={4} xl={3} className='position-relative'>
+                <Card onClick={this.handleClick} className={cardClass}>
+
+                    <Card.Img variant="top" src={this.props.book.img} />
+                    <Card.Body>
+
+                        <Card.Text className="text-truncate">
+                            {this.props.book.title}
+                        </Card.Text>
+                        <Card.Text>{this.props.book.price} €</Card.Text>
+                        <Button variant="primary">Buy</Button>
+                        {
+
+                            this.state.isSelected && <CommentArea elementId={this.props.book.asin} />
+                        }
+                    </Card.Body>
+                </Card>
+            </Col>
+
         )
-        
+
     }
-    }
+}
 
 
 export default SingleBook

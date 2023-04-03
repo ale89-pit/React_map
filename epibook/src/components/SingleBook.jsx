75a765e2dc54c708855d1db26dd38fec5ffa8248
ale89-pit <72412290+ale89-pit@users.noreply.github.com>
2023-04-03 18:29:05 +0200

    commit

--- epibook/src/components/SingleBook.jsx
+++ epibook/src/components/SingleBook.jsx
@@ -1,26 +1,41 @@
+import { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from "react-bootstrap/Col";
 
 
 
-function SingleBook (props){
-    return (
-        <Col key={props.book.asin} xs={6} lg={4} xl={3}>
-        <Card className="mb-2">
+class SingleBook extends Component {
+    state = {
+        isSelected : false,
+    }
+    handleClick = (e) => {
+        this.setState({
+            isSelected :!this.state.isSelected
+        })
+    }
+    render(){
+       let cardClass = this.state.isSelected ? "border" : "";
+        return (
+            <Col  onClick={this.handleClick} key={this.props.book.asin} xs={6} lg={4} xl={3}>
+            <Card className={"mb-2" + {cardClass}}>
+    
+                <Card.Img variant="top" src={this.props.book.img} />
+                <Card.Body>
+    
+                    <Card.Text className="text-truncate">
+                        {this.props.book.title}
+                    </Card.Text>
+                    <Card.Text>{this.props.book.price} €</Card.Text>
+                    <Button variant="primary">Buy</Button>
+                </Card.Body>
+            </Card>
+        </Col>
+        
+        )
+        
+    }
+    }
 
-            <Card.Img variant="top" src={props.book.img} />
-            <Card.Body>
-
-                <Card.Text className="text-truncate">
-                    {props.book.title}
-                </Card.Text>
-                <Card.Text>{props.book.price} €</Card.Text>
-                <Button variant="primary">Buy</Button>
-            </Card.Body>
-        </Card>
-    </Col>
-    )
-}
 
 export default SingleBook
